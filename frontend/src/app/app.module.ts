7e765d563a2e6a96e7968207b03b0b67bb02aa3e
miladfah <milad.fahimi@hiq.se>
2024-08-11 18:04:13 +0200

    FEAT [FRONT NGRX]: add ngrx first step packages

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [
@@ -13,7 +18,9 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,  // Routing module handles lazy loading
-    CoreModule         // CoreModule can include services and common components
+    CoreModule, StoreModule.forRoot(reducers, {
+      metaReducers
+    }), EffectsModule.forRoot([]), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }), StoreRouterConnectingModule.forRoot()         // CoreModule can include services and common components
   ],
   bootstrap: [AppComponent]
 })
