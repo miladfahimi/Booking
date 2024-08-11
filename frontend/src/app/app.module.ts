61212d319d1ccfc473e75dcf0e3dc9e84e1a3f5b
miladfah <milad.fahimi@hiq.se>
2024-08-11 20:32:32 +0200

    FIX [FRONT ANGULAR]: upgrade packeges

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -12,25 +12,19 @@ import { StoreRouterConnectingModule } from '@ngrx/router-store';
 // Import your application's reducers and metaReducers
 import { reducers, metaReducers } from './reducers';  // Assume you have a central place for reducers
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,  // Handles routing and lazy loading
-    CoreModule,  // CoreModule can include singleton services and common components
-
-    // NgRx Store Setup
-    StoreModule.forRoot(reducers, { metaReducers }),  // Global store configuration
-    EffectsModule.forRoot([]),  // Register global effects, if any
-    StoreDevtoolsModule.instrument({
-      maxAge: 25,  // Retain last 25 states
-      logOnly: !isDevMode(),  // Restrict extension to log-only mode in production
-    }),
-    StoreRouterConnectingModule.forRoot()  // Sync Angular Router with NgRx store
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule, // Handles routing and lazy loading
+        CoreModule, // CoreModule can include singleton services and common components
+        // NgRx Store Setup
+        StoreModule.forRoot(reducers, { metaReducers }), // Global store configuration
+        EffectsModule.forRoot([]), // Register global effects, if any
+        StoreDevtoolsModule.instrument({
+            maxAge: 25, // Retain last 25 states
+            logOnly: !isDevMode(), // Restrict extension to log-only mode in production
+        , connectInZone: true}),
+        StoreRouterConnectingModule.forRoot() // Sync Angular Router with NgRx store
+    ], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
