ef20d032f685cfa0d4740f4f652f4ee418993cfa
Milad Fahimi <milad.f@mail.com>
2025-11-22 13:03:02 +0100

    add a new basket lib

---
+++ frontend/tennis-time/libs/basket/src/lib/basket/basket.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-basket',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './basket.component.html',
+  styleUrl: './basket.component.css',
+})
+export class BasketComponent {}
